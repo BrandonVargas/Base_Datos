b4106a3ca73c3ffe848940f6d181bf89163b2912
ZtuX <alfredo.hdz.alarcon@gmail.com>
2014-06-07 16:16:21 -0500

    Cambios en el LogIn
    Se puede especificar el puerto y el servidor al cual conectarse...

--- src/interfaz_base_datos/Login.java
+++ src/interfaz_base_datos/Login.java
@@ -7,9 +7,6 @@
 package interfaz_base_datos;
 
 import conexion.sql.ConnectionSQL;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
-import java.util.Arrays;
 import javax.swing.JOptionPane;
 
 /**
@@ -17,7 +14,12 @@ import javax.swing.JOptionPane;
  * @author brandon
  */
 public class Login extends javax.swing.JFrame {
-
+    /*
+    * Variables en las que se fija un Servidor, puerto y base de datos por default para el servicio
+    **/
+    private static final String SERVER_BY_DEFAULT = "localhost";
+    private static final String PORT_BY_DEFAULT = "1521";
+    private static final String DATA_BASE_BY_DEFAULT = "CURSOBD";
     /**
      * Creates new form Login
      */
@@ -38,8 +40,13 @@ public class Login extends javax.swing.JFrame {
         jButton2 = new javax.swing.JButton();
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
-        jTextField1 = new javax.swing.JTextField();
-        jPasswordField1 = new javax.swing.JPasswordField();
+        tfUser = new javax.swing.JTextField();
+        tfPassword = new javax.swing.JPasswordField();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        tfServer = new javax.swing.JTextField();
+        tfPort = new javax.swing.JTextField();
+        jLabel5 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Iniciar Sesión");
@@ -59,12 +66,22 @@ public class Login extends javax.swing.JFrame {
         });
 
         jLabel1.setFont(new java.awt.Font("Ubuntu", 0, 18)); // NOI18N
-        jLabel1.setText("USUARIO");
+        jLabel1.setText("*USUARIO");
 
         jLabel2.setFont(new java.awt.Font("Ubuntu", 0, 18)); // NOI18N
-        jLabel2.setText("CONTRASEÑA");
+        jLabel2.setText("*CONTRASEÑA");
 
-        jTextField1.setName(""); // NOI18N
+        tfUser.setName(""); // NOI18N
+
+        jLabel3.setText("Servidor");
+
+        jLabel4.setText("Puerto");
+
+        tfServer.setText("localhost");
+
+        tfPort.setText("1521");
+
+        jLabel5.setText("*Campos Obligatorios");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -73,33 +90,60 @@ public class Login extends javax.swing.JFrame {
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jLabel1)
-                    .addComponent(jLabel2))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addGap(0, 0, Short.MAX_VALUE)
+                        .addComponent(jLabel3)
+                        .addGap(10, 10, 10)
+                        .addComponent(tfServer, javax.swing.GroupLayout.PREFERRED_SIZE, 112, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(jButton1)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 45, Short.MAX_VALUE)
-                        .addComponent(jButton2))
-                    .addComponent(jTextField1)
-                    .addComponent(jPasswordField1))
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jLabel1)
+                            .addComponent(jLabel2)
+                            .addComponent(jLabel5))
+                        .addGap(0, 0, Short.MAX_VALUE)))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(jButton1)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 45, Short.MAX_VALUE)
+                                .addComponent(jButton2))
+                            .addComponent(tfUser))
+                        .addGap(0, 0, Short.MAX_VALUE))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addGap(0, 0, Short.MAX_VALUE)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(tfPassword, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 191, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                                .addComponent(jLabel4)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                .addComponent(tfPort, javax.swing.GroupLayout.PREFERRED_SIZE, 49, javax.swing.GroupLayout.PREFERRED_SIZE)))))
+                .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jLabel1))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLabel1)
+                    .addComponent(tfUser, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel2)
-                    .addComponent(jPasswordField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(18, 18, 18)
+                    .addComponent(tfPassword, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jButton1)
-                    .addComponent(jButton2))
+                    .addComponent(jLabel3)
+                    .addComponent(jLabel4)
+                    .addComponent(tfServer, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(tfPort, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 15, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                        .addComponent(jButton1)
+                        .addComponent(jButton2))
+                    .addComponent(jLabel5, javax.swing.GroupLayout.Alignment.TRAILING))
                 .addContainerGap())
         );
 
@@ -108,25 +152,42 @@ public class Login extends javax.swing.JFrame {
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         //BOTON ACEPTAR
-        if(jTextField1.getText().length() >0 && jPasswordField1.getPassword().length >0){
-            //getPassword regresa el texto sin encriptar
-            //Creamos una nueva conexion
-
-            ConnectionSQL conn = new ConnectionSQL(jTextField1.getText(),
-                                                   jPasswordField1.getText(),
-                                                   "192.168.1.133",
-                                                   1521,"CURSOBD");
-
-            if(conn.getConnection()!=null){
-                this.dispose();
-                Usuario u = new Usuario();
-                u.mostrar(conn);
-            }else{
-                JOptionPane.showMessageDialog(null,"Usuario y/o contraseña inválidos");
-            }
-        }else{
-            JOptionPane.showMessageDialog(null,"ERROR!\nDebes llenar todos los campos");
+        if(tfServer.getText().equals("")){
+            tfServer.setText(SERVER_BY_DEFAULT);
         }
+        if(tfPort.getText().equals("")){
+            tfPort.setText(PORT_BY_DEFAULT);
+        }
+        try{
+            Integer.parseInt(tfPort.getText());
+            if(tfUser.getText().length() >0 && tfPassword.getPassword().length >0){
+                //getPassword regresa el texto sin encriptar
+                //Creamos una nueva conexion
+                /*
+                Si no se escribe nada en el campo de host se coloca "localhost" por default
+                */
+                
+                ConnectionSQL conn = new ConnectionSQL(tfUser.getText(),
+                                                   tfPassword.getText(),
+                                                   tfServer.getText(),
+                                                   Integer.parseInt(tfPort.getText()),
+                                                   DATA_BASE_BY_DEFAULT);
+
+                if(conn.getConnection()!=null){
+                    this.dispose();
+                    Usuario u = new Usuario();
+                    u.mostrar(conn);
+                }else{
+                    JOptionPane.showMessageDialog(null,"Usuario y/o contraseña inválidos");
+                }
+            }else{
+                JOptionPane.showMessageDialog(null,"ERROR!\nDebes llenar todos los campos");
+        }
+        }catch(NumberFormatException e){
+            JOptionPane.showMessageDialog(null,"Puerto inválido");
+        }
+        
+        
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
@@ -175,7 +236,12 @@ public class Login extends javax.swing.JFrame {
     private javax.swing.JButton jButton2;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
-    private javax.swing.JPasswordField jPasswordField1;
-    private javax.swing.JTextField jTextField1;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JPasswordField tfPassword;
+    private javax.swing.JTextField tfPort;
+    private javax.swing.JTextField tfServer;
+    private javax.swing.JTextField tfUser;
     // End of variables declaration//GEN-END:variables
 }
